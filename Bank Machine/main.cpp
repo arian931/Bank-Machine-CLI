da4457b4898c9fe6b1c8c2066226799445d6d173
Arian <arianhelgi@gmail.com>
2019-05-17 11:17:15 -0400

    Initial Commit

---
+++ Bank Machine/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Bank Machine
+//
+//  Created by Arian Taherzadeh on 2019-05-17.
+//  Copyright © 2019 Arian Taherzadeh. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
